da94cd9415a659d6dc727966bd027bfceb93ef61
Meng-Yun Wang <mengyun.wang@uib.no>
2022-02-15 15:42:36 +0100

    Add files via upload

---
+++ MRS_relocated.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+# Created by M.Y. WANG
+
+
+# Organize the mrs data
+
+filepath=/Users/wang/Desktop/Research_projects/BBSC/Functional/Data/Raw_data
+cd $filepath
+MRS_path=/Users/wang/Desktop/Research_projects/BBSC/MRS/Raw_data
+
+ls | grep -i sub- > ./subjList.txt # command grep is trying to filter the input with 'sub-'. -i ignore the high or low cases 
+
+for sub in `cat ./subjList.txt`; do 
+    mkdir $MRS_path/$sub
+    mkdir $MRS_path/$sub/Pfile
+    mkdir $MRS_path/$sub/anat 
+
+    ls ./$sub > ./sesList.txt #list files with 'ses-' in sub directories to sesList.txt 
+    ses_num=0
+    for ses_name in `cat ./sesList.txt`; do
+        
+        ((ses_num=$ses_num+1))
+        
+        cp $filepath/$sub/$ses_name/004_PROBE-SV_35/P*.7 $filepath/$sub/$ses_name/004_PROBE-SV_35/${sub}_${ses_num}_.7
+        mv $filepath/$sub/$ses_name/004_PROBE-SV_35/${sub}_${ses_num}_.7 $MRS_path/$sub/Pfile 
+
+        dcm2niix -b n -f ${sub}_${ses_num}_T1W -o $MRS_path/$sub/anat $filepath/$sub/$ses_name/003_Sag_FSPGR_3D/ 
+
+    done
+    
+done
